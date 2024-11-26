7e9aca977f47c64c715c333e2aa3896341d3a86c
August Garibay <august.garibay1@gmail.com>
2024-11-26 14:19:02 -0700

    Fine tuned model

--- ML/ml_starter/yolo.py
+++ ML/ml_starter/yolo.py
@@ -25,48 +25,48 @@ if str(ROOT) not in sys.path:
 if platform.system() != "Windows":
     ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-from models.common import (
-    C3,
-    C3SPP,
-    C3TR,
-    SPP,
-    SPPF,
-    Bottleneck,
-    BottleneckCSP,
-    C3Ghost,
-    C3x,
-    Classify,
-    Concat,
-    Contract,
-    Conv,
-    CrossConv,
-    DetectMultiBackend,
-    DWConv,
-    DWConvTranspose2d,
-    Expand,
-    Focus,
-    GhostBottleneck,
-    GhostConv,
-    Proto,
-)
-from models.experimental import MixConv2d
-from utils.autoanchor import check_anchor_order
-from utils.general import LOGGER, check_version, check_yaml, colorstr, make_divisible, print_args
-from utils.plots import feature_visualization
-from utils.torch_utils import (
-    fuse_conv_and_bn,
-    initialize_weights,
-    model_info,
-    profile,
-    scale_img,
-    select_device,
-    time_sync,
-)
+# from models.common import (
+#     C3,
+#     C3SPP,
+#     C3TR,
+#     SPP,
+#     SPPF,
+#     Bottleneck,
+#     BottleneckCSP,
+#     C3Ghost,
+#     C3x,
+#     Classify,
+#     Concat,
+#     Contract,
+#     Conv,
+#     CrossConv,
+#     DetectMultiBackend,
+#     DWConv,
+#     DWConvTranspose2d,
+#     Expand,
+#     Focus,
+#     GhostBottleneck,
+#     GhostConv,
+#     Proto,
+# )
+# from models.experimental import MixConv2d
+# from utils.autoanchor import check_anchor_order
+# from utils.general import LOGGER, check_version, check_yaml, colorstr, make_divisible, print_args
+# from utils.plots import feature_visualization
+# from utils.torch_utils import (
+#     fuse_conv_and_bn,
+#     initialize_weights,
+#     model_info,
+#     profile,
+#     scale_img,
+#     select_device,
+#     time_sync,
+# )
 
-try:
-    import thop  # for FLOPs computation
-except ImportError:
-    thop = None
+# try:
+#     import thop  # for FLOPs computation
+# except ImportError:
+#     thop = None
 
 
 class Detect(nn.Module):
